1f4001884297dddcc6ff6662b8adbf9d2153b524
Boubix88 <86961375+Boubix88@users.noreply.github.com>
2022-10-29 01:31:15 +0200

    Version 0.3
    -Boutons changent de couleurs
    -Ajout photo de profil

--- fonctions.js
+++ fonctions.js
@@ -1,12 +1,33 @@
-
 var navbar = document.getElementById("bandeau");
 var menu = document.getElementById("navigation");
+const btnAccueil = document.getElementById("boutonAccueil");
+const btnProjets = document.getElementById("boutonProjet");
+const btnAPropos = document.getElementById("boutonAPropos");
+const btnContacts = document.getElementById("boutonContacts");
+const accueil = document.getElementById("accueil");
+const projets = document.getElementById("projets");
+const aPropos = document.getElementById("aPropos");
+const contacts = document.getElementById("contacts");
 window.onscroll = function(){
     if (window.pageYOffset >= menu.offsetTop){
-        console.log("Oui");
         navbar.classList.add("sticky");
     } else {
-        console.log("Non");
         navbar.classList.remove("sticky");
     }
+
+	setBtnStyle(window, accueil, btnAccueil);
+	setBtnStyle(window, projets, btnProjets);
+	setBtnStyle(window, aPropos, btnAPropos);
+	setBtnStyle(window, contacts, btnContacts);
+}
+
+function setBtnStyle(window, element, btn){
+	if (window.pageYOffset >= element.offsetTop && window.pageYOffset < (element.offsetTop + element.offsetHeight)){
+        btn.style.background = "blanchedalmond";
+		btn.style.color = "black";
+    } else {
+        btn.style.background = "transparent";
+		btn.style.color = "blanchedalmond";
+    }
+	return;
 }
