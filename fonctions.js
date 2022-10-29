7a7b3f56c8a189b4c5f58643623c18064884193d
Boubix88 <86961375+Boubix88@users.noreply.github.com>
2022-10-29 21:51:57 +0200

    0.6
    -Ajout d'animations
    -Ajout d'un projet
    -Changement des couleurs
    -Ajout d'un manifest

--- fonctions.js
+++ fonctions.js
@@ -21,6 +21,22 @@ window.onscroll = function(){
 	setBtnStyle(window, contacts, btnContacts);
 }
 
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+}
+window.addEventListener("scroll", reveal);
+
 function setBtnStyle(window, element, btn){
 	if (window.pageYOffset >= element.offsetTop && window.pageYOffset < (element.offsetTop + element.offsetHeight)){
         btn.style.background = "blanchedalmond";
